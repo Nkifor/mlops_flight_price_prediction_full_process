d8567e4bc358936127d89e31b145d511ba8791c6
Krzysztof <stasiek3000@gmail.com>
2023-08-14 05:39:13 +0200

    maintenance commit after dvc customized in S3

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -4,6 +4,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import load_object
+import dvc.api
 
 
 class PredictionPipeline:
@@ -12,15 +13,28 @@ class PredictionPipeline:
 
     def predict(self,features):
         try:
-            model_path =os.path.join('artifacts', 'model.pkl')
-            preprocessor_path = os.path.join('artifacts','proprocessor.pkl')
+            local_model_path =os.path.join('artifacts', 'model.pkl')
+            model_path = 's3://mlopsflightpricepredictionartifacts/model.pkl'
+            local_preprocessor_path = os.path.join('artifacts','proprocessor.pkl')
+            preprocessor_path = 's3://mlopsflightpricepredictionartifacts/proprocessor.pkl'
+
+            with dvc.api.open(model_path) as f_model:
+                with open(local_model_path, 'wb') as local_model_file:
+                    local_model_file.write(f_model.read())
+
+            with dvc.api.open(preprocessor_path) as f_preprocessor:
+                with open(local_preprocessor_path, 'wb') as local_preprocessor_file:
+                    local_preprocessor_file.write(f_preprocessor.read())
+
             print("Before Loading")
             model=load_object(file=model_path)
             preprocessor=load_object(file=preprocessor_path)
+
             print("After Loading")
             data_scaled=preprocessor.transform(features)
             model_prediction=model.predict(data_scaled)
             return model_prediction
+
         except Exception as e:
             raise CustomException(e,sys)
 

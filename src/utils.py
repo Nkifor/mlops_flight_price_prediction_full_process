ff33e043cca9200e2d0c3b4e994abdd6c7c1fc6f
Krzysztof <stasiek3000@gmail.com>
2023-08-16 15:02:28 +0200

    final model run

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+from cachetools import LRUCache
 
 from src.exception import CustomException
 
@@ -68,19 +69,38 @@ def load_object(file):
         raise CustomException(e, sys)
 
 
+
+
+from cachetools import LRUCache
+
+cache = LRUCache(maxsize=100)
+
 def load_compressed_object(file):
-    try:                                                     #First  compression approach
-        with bz2.BZ2File(file, 'rb') as file_object:
-            return pickle.load(file_object)
-    except Exception as e:
-        raise CustomException(e, sys)
-    #try:                                                      #Second compression approach
-    #    with open(file, 'rb') as file_object:
-    #        decomp = bz2.BZ2Decompressor()
-    #        data = file_object.read()
-    #        decompressed_data = decomp.decompress(data)
-    #        return pickle.loads(decompressed_data)
+    if file in cache:
+        return cache[file]
+    else:
+        with bz2.BZ2File(file, 'rb') as f:
+            obj = pickle.load(f)
+            cache[file] = obj
+            return obj
 
 
-    except Exception as e:
-        raise CustomException(e, sys)
+
+
+
+#def load_compressed_object(file):
+#    try:                                                     #First  compression approach
+#        with bz2.BZ2File(file, 'rb') as file_object:
+#            return pickle.load(file_object)
+#    #except Exception as e:
+#    #    raise CustomException(e, sys)
+#    #try:                                                      #Second compression approach
+#    #    with open(file, 'rb') as file_object:
+#    #        decomp = bz2.BZ2Decompressor()
+#    #        data = file_object.read()
+#    #        decompressed_data = decomp.decompress(data)
+#    #        return pickle.loads(decompressed_data)
+#
+#
+#    except Exception as e:
+#        raise CustomException(e, sys)

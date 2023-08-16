2494cbe9f45d04284349b3c4644efcc885a319e9
Krzysztof <stasiek3000@gmail.com>
2023-08-16 14:28:09 +0200

    final model run

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -4,7 +4,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import load_object, load_compressed_object
-import dvc.api
+
 
 
 class PredictionPipeline:

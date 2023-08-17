affcf34ed590ea5d876735da85b20df6f1d192ab
Krzysztof <stasiek3000@gmail.com>
2023-08-17 07:41:32 +0200

    uWSGI and NGINX added configuration

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ def predict_datapoint():
         print("After Prediction")
         return render_template('home.html',results=rounded_results)
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True) #AWS build
+    app.run()
+    #app.run(host="0.0.0.0", port=8080, debug=True) #AWS build
     #app.run(host="0.0.0.0", debug=True) #local - for testing
 
 
